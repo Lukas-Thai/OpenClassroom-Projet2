337493aa5d52bbc0ce052e8d672d837a4b5e0325
BuildTools <unconfigured@null.spigotmc.org>
2025-04-03 10:36:21 +0200

    first commit

---
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -0,0 +1,116 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, Observable, Subject, takeUntil } from 'rxjs';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from '../../core/models/Olympic';
+import { Participation } from '../../core/models/Participation';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from '../home/home.component';
+import Chart, { ChartConfiguration, ChartItem } from 'chart.js/auto';
+
+@Component({
+  selector: 'app-country-detail',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './country-detail.component.html',
+  styleUrl: './country-detail.component.scss'
+})
+
+export class CountryDetailComponent implements OnInit{
+  private countryId!: number;
+  public olympics$ !: Observable<Olympic>;
+  private destroy$ = new Subject<void>();
+  private rawData : Array<Olympic> = []; 
+  public countryName:string = "Fetching...";
+  public nbEntry:number = 0;
+  public nbMedal:number = 0;
+  public nbAthlete:number = 0;
+  chart !:Chart;
+  chartConfig!: ChartConfiguration;
+  constructor(private olympicService: OlympicService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Récupère l'id du pays
+    this.route.paramMap.subscribe((params) => {
+      const paramId :string | null = params.get('id');
+      try{
+        if(!paramId){//pas d'id
+          throw new Error();
+        }
+        this.countryId = parseInt(paramId as string);
+      }
+      catch(e){
+        this.countryName = "Error";
+      }
+    });
+
+    this.olympics$ = this.olympicService.getOlympics();
+      this.olympicService.loadInitialData();
+      this.olympics$.pipe(
+        filter(data => data != null), 
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        next: (data) => {
+          this.addToRawData(data);
+          this.updateGraph();
+        },
+        error: (err) => {
+          console.error('Erreur lors de la récupération des données :', err);
+        },
+        complete: () => {
+        }
+      });
+  }
+  updateGraph(){
+    if(this.rawData.length > 0){
+      this.chartConfig = {type : "line",
+        data : {labels : this.rawData[0].participations.map((item: Participation) => item.year),
+        datasets: [{label: "Medals gained", data:this.rawData[0].participations.map((item: Participation) => item.medalsCount)}]
+      },
+      options:{
+        responsive: true,
+        maintainAspectRatio: false ,
+        scales: {
+          y: { 
+            beginAtZero: true
+          }
+        }
+      }}
+      if(this.chart){
+        this.chart.destroy();
+      }
+      this.chart = new Chart(document.getElementById('graph') as ChartItem,this.chartConfig);//on refait un nouveau graphe
+    }
+
+  }
+  addToRawData(newData: Olympic[] | Olympic) {
+    const arrayData: Olympic[] = Array.isArray(newData) ? newData : [newData]; //conversion des données en une liste
+    arrayData.forEach((data:Olympic) => {
+      if(data.id === this.countryId){//on ignore tout les pays qui correspondent pas à l'id recherché
+        if(this.rawData.length === 0){
+          this.rawData.push(data);
+          this.countryName = this.rawData[0].country;
+        }
+        else{
+          for(let participation of data.participations){
+            const participationExists = this.rawData[0].participations.some(
+              (item: Participation) => item.id === participation.id
+            );
+            if (!participationExists) {//la participation n'existe pas on l'ajoute
+              this.rawData[0].participations.push(participation);
+            }
+          }
+        }
+        this.nbEntry = this.rawData[0].participations.length;
+        this.nbMedal = this.rawData[0].participations.reduce((sum, p) => sum + p.medalsCount, 0);
+        this.nbAthlete = this.rawData[0].participations.reduce((sum, p) => sum + p.athleteCount, 0);
+      }
+    });
+  }
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { filter, Observable, of, Subject, takeUntil } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import Chart, { ChartConfiguration, ChartData, ChartItem, ChartOptions } from 'chart.js/auto';
+import { Olympic } from '../../core/models/Olympic';
+import { Router } from '@angular/router';
+import { Participation } from '../../core/models/Participation';
+
+
 
 @Component({
   selector: 'app-home',
@@ -8,11 +14,87 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
-
-  constructor(private olympicService: OlympicService) {}
+  public olympics$ !: Observable<Olympic>;
+  private destroy$ = new Subject<void>();
+  private rawData : Array<Olympic> = []; 
+  private pieChart !: Chart;
+  public nbJo:number = 0;
+  public nbCountry:number = 0;
+  chart !:ChartConfiguration;
+  constructor(private olympicService: OlympicService, private router: Router) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.loadInitialData();
+    this.olympics$.pipe(
+      filter(data => data != null), // filtre les undefined ou null
+      takeUntil(this.destroy$)
+    )
+    .subscribe({
+      next: (data) => {//récupération des données
+        this.addToRawData(data);
+        this.updateGraph();
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des données :', err);
+      },
+      complete: () => {
+      }
+    });
+    this.updateGraph();
+  }
+  updateGraph(){//fonction qui gère la mise à jour du graphique
+    this.chart = {type: "pie",
+      data:{
+        labels:this.rawData.map((item:Olympic) => item.country ),
+        datasets:[{
+          label: "Medals ",
+          data: this.rawData.map((item:Olympic) => item.participations.reduce((sum, p) => sum + p.medalsCount, 0))
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        onClick: (_event: any, activeElements: any) => { //gestion du click sur un pays
+          if (activeElements && activeElements.length > 0) {
+            const index:number = activeElements[0].index;
+            const countryId:number = this.rawData[index].id; // Récupération de l'id du pays
+            this.router.navigate(['/country', countryId]); // Navigation vers la page de détails du pays
+          }
+        }
+      }
+    }
+    if(this.rawData.length!=0){//pour éviter un undefined
+      this.nbJo = this.rawData[0].participations.length;
+      this.nbCountry = this.rawData.length;
+    }
+    if(this.pieChart != null){//on détruit le graphe déjà existant
+      this.pieChart.destroy();
+    }
+    this.pieChart = new Chart(document.getElementById('graph') as ChartItem,this.chart);//on refait un nouveau graphe
+  }
+  addToRawData(newData: Olympic[] | Olympic) {
+    const arrayData: Olympic[] = Array.isArray(newData) ? newData : [newData]; //conversion des données en une liste
+    const rawDataMap = new Map(this.rawData.map(item => [item.id, item])); //utilisation d'une map pour accélérer les recherches
+    arrayData.forEach(data => {
+      const existingData = rawDataMap.get(data.id); //recherche du pays dans la map
+      if (existingData) {//cas où il existe déjà, il faut ajouter les participations non existantes
+        data.participations.forEach(participation => {
+          const participationExists = existingData.participations.some(
+            (item: Participation) => item.id === participation.id
+          );
+          if (!participationExists) {//la participation n'existe pas on l'ajoute
+            existingData.participations.push(participation);
+          }
+        });
+      } else {//le pays n'existe pas dans notre liste, on l'ajoute
+        this.rawData.push(data);
+        rawDataMap.set(data.id, data);
+      }
+    });
+  }
+  ngOnDestroy() { //on ferme les observables
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
